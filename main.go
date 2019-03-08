6cab4bb89a084937a2d03389886e417871ff1a2f
dudk <serg.dudk@gmail.com>
2019-03-08 08:09:20 +0100

    convert: add radiobuttons to index and template injection

--- main.go
+++ main.go
@@ -1,33 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
 var (
-	loginTemplate *template.Template
+	indexTemplate = template.Must(template.ParseFiles("web/index.tmpl"))
+
+	convertForm = &ConvertForm{
+		Formats: map[string]string{
+			"wav": "wav",
+			"mp3": "mp3",
+		},
+	}
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		loginTemplate.Execute(w, nil)
-	case http.MethodPost:
-		r.ParseForm()
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
+// ConvertForm provides a form for a user to define conversion parameters.
+type ConvertForm struct {
+	Formats map[string]string
+}
+
+func indexHandler(indexTemplate *template.Template) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			indexTemplate.Execute(w, convertForm)
+		case http.MethodPost:
+			r.ParseForm()
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
 }
 
 func main() {
-	loginTemplate, _ = template.ParseFiles("web/index.tmpl")
 	// setting router rule
-	http.HandleFunc("/", index)
+	http.Handle("/", indexHandler(indexTemplate))
 	err := http.ListenAndServe(":8080", nil) // setting listening port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)

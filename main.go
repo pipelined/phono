677f8dc2630dbb6d4522960c8dca81b9ffedf0aa
dudk <serg.dudk@gmail.com>
2019-03-11 19:37:50 +0100

    convert: detect input format

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 var (
@@ -17,18 +19,47 @@ var (
 	}
 )
 
+const (
+	maxInputSize = 2 * 1024 * 1024
+	tmpPath      = "tmp"
+)
+
 // ConvertForm provides a form for a user to define conversion parameters.
 type ConvertForm struct {
 	Formats map[string]string
 }
 
-func indexHandler(indexTemplate *template.Template) http.Handler {
+func convertHandler(indexTemplate *template.Template, maxSize int64, path string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
 			indexTemplate.Execute(w, convertForm)
 		case http.MethodPost:
-			r.ParseForm()
+			// check max size
+			r.Body = http.MaxBytesReader(w, r.Body, maxSize)
+			if err := r.ParseMultipartForm(maxSize); err != nil {
+				http.Error(w, "File too big", http.StatusBadRequest)
+				return
+			}
+			// obtain file handler
+			file, handler, err := r.FormFile("convertfile")
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Invalid file %v", err), http.StatusBadRequest)
+				return
+			}
+			defer file.Close()
+
+			inFormat := filepath.Ext(handler.Filename)
+			fmt.Printf("inFormat: %s\n", inFormat)
+
+			outFormat := r.FormValue("format")
+			switch outFormat {
+			case "wav":
+			case "mp3":
+			default:
+				http.Error(w, fmt.Sprintf("Invalid output file format: %v", outFormat), http.StatusBadRequest)
+				return
+			}
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
@@ -37,7 +68,7 @@ func indexHandler(indexTemplate *template.Template) http.Handler {
 
 func main() {
 	// setting router rule
-	http.Handle("/", indexHandler(indexTemplate))
+	http.Handle("/", convertHandler(indexTemplate, maxInputSize, tmpPath))
 	err := http.ListenAndServe(":8080", nil) // setting listening port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)

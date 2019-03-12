4f92fe2419a0e9ad87bb625cf2f3131b83832400
dudk <serg.dudk@gmail.com>
2019-03-12 07:48:22 +0100

    convert: add content download

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/pipelined/convert
 
 go 1.12
+
+require (
+	github.com/pipelined/mp3 v0.0.0-20190310190454-6340e5bcf1ae
+	github.com/pipelined/pipe v0.4.0
+	github.com/pipelined/signal v0.0.0-20190303105250-40bacde8022c
+	github.com/pipelined/wav v0.0.0-20190310183337-020d3be76c40
+)
--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"log"
+	"mime"
 	"net/http"
+	"os"
 	"path/filepath"
+	"strconv"
+
+	"github.com/pipelined/mp3"
+	"github.com/pipelined/pipe"
+	"github.com/pipelined/signal"
+	"github.com/pipelined/wav"
 )
 
 var (
@@ -13,8 +22,8 @@ var (
 
 	convertForm = &ConvertForm{
 		Formats: map[string]string{
-			"wav": "wav",
-			"mp3": "mp3",
+			"wav": ".wav",
+			"mp3": ".mp3",
 		},
 	}
 )
@@ -44,22 +53,83 @@ func convertHandler(indexTemplate *template.Template, maxSize int64, path string
 			// obtain file handler
 			file, handler, err := r.FormFile("convertfile")
 			if err != nil {
-				http.Error(w, fmt.Sprintf("Invalid file %v", err), http.StatusBadRequest)
+				http.Error(w, fmt.Sprintf("Invalid file: %v", err), http.StatusBadRequest)
 				return
 			}
 			defer file.Close()
 
+			// create pump for input format
+			var pump pipe.Pump
 			inFormat := filepath.Ext(handler.Filename)
-			fmt.Printf("inFormat: %s\n", inFormat)
+			switch inFormat {
+			case ".wav":
+				pump = wav.NewPump(file)
+			case ".mp3":
+				pump = mp3.NewPump(file)
+			default:
+				http.Error(w, fmt.Sprintf("Invalid input file format: %v", inFormat), http.StatusBadRequest)
+				return
+			}
 
+			// create sink for output format
+			var sink pipe.Sink
 			outFormat := r.FormValue("format")
+			tmpFileName := path + "/" + handler.Filename
+			var tmpFile *os.File
 			switch outFormat {
-			case "wav":
-			case "mp3":
+			case ".wav":
+				tmpFile, err = os.Create(tmpFileName)
+				if err != nil {
+					http.Error(w, fmt.Sprintf("Error creating temp file: %v", err), http.StatusInternalServerError)
+				}
+				sink = wav.NewSink(tmpFile, signal.BitDepth24)
+			case ".mp3":
 			default:
 				http.Error(w, fmt.Sprintf("Invalid output file format: %v", outFormat), http.StatusBadRequest)
 				return
 			}
+
+			// build convert pipe
+			convert, err := pipe.New(1024, pipe.WithPump(pump), pipe.WithSinks(sink))
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Failed to build pipe: %v", err), http.StatusInternalServerError)
+				return
+			}
+
+			// run conversion
+			err = pipe.Wait(convert.Run())
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Failed to build pipe: %v", err), http.StatusInternalServerError)
+				return
+			}
+			err = tmpFile.Close()
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Failed to close temp file: %v", err), http.StatusInternalServerError)
+				return
+			}
+
+			tmpFile, err = os.Open(tmpFileName)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Failed to re-open temp file: %v", err), http.StatusInternalServerError)
+				return
+			}
+			stat, err := tmpFile.Stat()
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Failed to get file stats: %v", err), http.StatusInternalServerError)
+				return
+			}
+			fileSize := strconv.FormatInt(stat.Size(), 10)
+			//Send the headers
+			w.Header().Set("Content-Disposition", "attachment; filename="+tmpFileName)
+			w.Header().Set("Content-Type", mime.TypeByExtension(outFormat))
+			w.Header().Set("Content-Length", fileSize)
+			io.Copy(w, tmpFile) // send file to a client
+			err = tmpFile.Close()
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Failed to close temp file: %v", err), http.StatusInternalServerError)
+				return
+			}
+			return
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
 		}

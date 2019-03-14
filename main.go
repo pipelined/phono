18dedf560cc8e79ee3dadb2226ff60b9ae2874b0
dudk <serg.dudk@gmail.com>
2019-03-14 19:53:06 +0100

    convert: extract conversion method

--- main.go
+++ main.go
@@ -50,6 +50,12 @@ type ConvertForm struct {
 // Format is a file extension.
 type Format string
 
+type source interface {
+	io.Reader
+	io.Seeker
+	io.Closer
+}
+
 // convertHandler converts form files to the format provided y form.
 func convertHandler(indexTemplate *template.Template, maxSize int64, path string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -70,23 +76,10 @@ func convertHandler(indexTemplate *template.Template, maxSize int64, path string
 				return
 			}
 			defer file.Close()
+			inFormat := Format(filepath.Ext(handler.Filename))
 
-			// create pump for input format
-			var pump pipe.Pump
-			inFormat := filepath.Ext(handler.Filename)
-			switch Format(inFormat) {
-			case WavFormat:
-				pump = wav.NewPump(file)
-			case Mp3Format:
-				pump = mp3.NewPump(file)
-			default:
-				http.Error(w, fmt.Sprintf("Invalid input file format: %v", inFormat), http.StatusBadRequest)
-				return
-			}
-
-			// create sink for output format
-			var sink pipe.Sink
-			outFormat := r.FormValue("format")
+			// create temp file
+			outFormat := Format(r.FormValue("format"))
 			tmpFileName := tmpFileName(path)
 			tmpFile, err := os.Create(tmpFileName)
 			if err != nil {
@@ -95,35 +88,20 @@ func convertHandler(indexTemplate *template.Template, maxSize int64, path string
 			}
 			defer cleanUp(tmpFile)
 
-			switch outFormat {
-			case ".wav":
-				sink = wav.NewSink(tmpFile, signal.BitDepth24)
-			case ".mp3":
-				sink = mp3.NewSink(tmpFile, 192, 10)
-			default:
-				http.Error(w, fmt.Sprintf("Invalid output file format: %v", outFormat), http.StatusBadRequest)
-				return
-			}
-
-			// build convert pipe
-			convert, err := pipe.New(1024, pipe.WithPump(pump), pipe.WithSinks(sink))
+			// convert file using temp file
+			err = convert(file, tmpFile, inFormat, outFormat)
 			if err != nil {
-				http.Error(w, fmt.Sprintf("Failed to build pipe: %v", err), http.StatusInternalServerError)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
 
-			// run conversion
-			err = pipe.Wait(convert.Run())
-			if err != nil {
-				http.Error(w, fmt.Sprintf("Failed to execute pipe: %v", err), http.StatusInternalServerError)
-				return
-			}
-
+			// reset temp file
 			_, err = tmpFile.Seek(0, 0)
 			if err != nil {
 				http.Error(w, fmt.Sprintf("Failed to reset temp file: %v", err), http.StatusInternalServerError)
 				return
 			}
+			// get temp file stats for headers
 			stat, err := tmpFile.Stat()
 			if err != nil {
 				http.Error(w, fmt.Sprintf("Failed to get file stats: %v", err), http.StatusInternalServerError)
@@ -132,7 +110,7 @@ func convertHandler(indexTemplate *template.Template, maxSize int64, path string
 			fileSize := strconv.FormatInt(stat.Size(), 10)
 			//Send the headers
 			w.Header().Set("Content-Disposition", "attachment; filename="+outFileName(handler.Filename, inFormat, outFormat))
-			w.Header().Set("Content-Type", mime.TypeByExtension(outFormat))
+			w.Header().Set("Content-Type", mime.TypeByExtension(string(outFormat)))
 			w.Header().Set("Content-Length", fileSize)
 			io.Copy(w, tmpFile) // send file to a client
 			return
@@ -157,8 +135,8 @@ func tmpFileName(path string) string {
 }
 
 // outFileName return output file name. It replaces input format extension with output.
-func outFileName(name, oldExt, newExt string) string {
-	return strings.Replace(strings.ToLower(name), oldExt, newExt, 1)
+func outFileName(name string, oldExt, newExt Format) string {
+	return strings.Replace(strings.ToLower(name), string(oldExt), string(newExt), 1)
 }
 
 // cleanUp removes temporary file and handles all errors on the way.
@@ -171,4 +149,41 @@ func cleanUp(f *os.File) {
 	if err != nil {
 		log.Printf("Failed to delete temp file")
 	}
+}
+
+func convert(s source, destination *os.File, inFormat, outFormat Format) error {
+	// create pump for input format
+	var pump pipe.Pump
+	switch inFormat {
+	case WavFormat:
+		pump = wav.NewPump(s)
+	case Mp3Format:
+		pump = mp3.NewPump(s)
+	default:
+		return fmt.Errorf("Invalid input file format: %v", inFormat)
+	}
+
+	// create sink for output format
+	var sink pipe.Sink
+	switch outFormat {
+	case ".wav":
+		sink = wav.NewSink(destination, signal.BitDepth24)
+	case ".mp3":
+		sink = mp3.NewSink(destination, 192, 10)
+	default:
+		return fmt.Errorf("Invalid output file format: %v", outFormat)
+	}
+
+	// build convert pipe
+	convert, err := pipe.New(1024, pipe.WithPump(pump), pipe.WithSinks(sink))
+	if err != nil {
+		return fmt.Errorf("Failed to build pipe: %v", err)
+	}
+
+	// run conversion
+	err = pipe.Wait(convert.Run())
+	if err != nil {
+		return fmt.Errorf("Failed to execute pipe: %v", err)
+	}
+	return nil
 }
